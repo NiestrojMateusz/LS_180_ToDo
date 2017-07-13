17b169721f8635a5f7051886568e85be45d86394
Mateusz Niestrój <niestroj.mateusz@gmail.com>
2017-07-13 10:56:47 +0000

    Completed database todo app

--- todo.rb
+++ todo.rb
@@ -1,13 +1,19 @@
 require "sinatra"
-require "sinatra/reloader" if development?
 require "sinatra/content_for"
 require "tilt/erubis"
+require_relative "database_persistance"
 
 # Set up sinatra to use sessions
 configure do
   enable :sessions # tells sinatra to enable a sessions support
   set :session_secret, 'secret' # setting session secret, for production this should be something longer and not straightforward in the code
   set :erb, :escape_html => true
+  
+end
+
+configure(:development) do
+  require "sinatra/reloader"
+  also_reload "database_persistance.rb"
 end
 
 helpers do
@@ -42,69 +48,8 @@ helpers do
   end
 end
 
-class SessionPersistance
-
-  def initialize(session)
-    @session = session
-    @session[:lists] ||= []
-  end
-
-  def find_list(id)
-    @session[:lists].find { |list| list[:id] == id }
-  end
-
-  def all_lists
-    @session[:lists]
-  end
-
-  def create_new_list(list_name)
-    id = next_element_id(@session[:lists])
-    @session[:lists] << { id: id, name: list_name, todos: [] }
-  end
-
-  def delete_list(id)
-    @session[:lists].reject! { |list| list[:id] == id }
-  end
-
-  def update_list_name(id, new_name)
-    list = find_list(id)
-    list[:name] = new_name
-  end
-
-  def create_new_todo(list_id, todo_name)
-    list = find_list(list_id)
-    id = next_element_id(list[:todos])
-    list[:todos] << {id: id, name: todo_name, completed: false}
-  end
-
-  def delete_todo_from_list(list_id, todo_id)
-    list = find_list(list_id)
-    list[:todos].reject! {|todo| todo[:id] == todo_id}
-  end
-
-  def update_todo_status(list_id, todo_id, new_status)
-    list = find_list(list_id)
-    todo = list[:todos].find {|t| t[:id] == todo_id}
-    todo[:completed] = new_status
-  end
-
-  def mark_all_todos_as_completed(list_id)
-    list = find_list(list_id)
-    list[:todos].each do |todo|
-      todo[:completed] = true
-    end
-  end
-
-  private
-
-  def next_element_id(elements)
-    max = elements.map { |element| element[:id] }.max || 0
-    max + 1
-  end
-end
-
 before do
-  @storage = SessionPersistance.new(session)
+  @storage = DatabasePersistance.new(logger)
 end
 
 def load_list(id)
@@ -146,15 +91,15 @@ end
 post "/lists" do
   list_name = params[:list_name].strip
 
-error = error_for_list_name(list_name)
-  if error
-    session[:error] = error
-    erb :new_list, layout: :layout
-  else
-    @storage.create_new_list(list_name)
-    session[:success] = "The list has been created"
-    redirect "/lists"
-  end
+  error = error_for_list_name(list_name)
+    if error
+      session[:error] = error
+      erb :new_list, layout: :layout
+    else
+      @storage.create_new_list(list_name)
+      session[:success] = "The list has been created"
+      redirect "/lists"
+    end
 end
 
 # View a single todo list
